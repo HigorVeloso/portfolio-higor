c60c123ecf4a38f3d6f8a9af8c4060138f74656c
Higor Veloso <higorchdv1@outlook.com>
2024-02-21 17:44:55 -0300

    inserindo o curriculo no corpo

--- src/componentes/Corpo/index.jsx
+++ src/componentes/Corpo/index.jsx
@@ -1,14 +1,30 @@
 import React from "react";
+import { pdfjs } from 'react-pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 const Corpo = () => {
+    const downloadPDF = () => {
+        const pdfUrl = '/arquivo/curriculo_higor-veloso.pdf';
+        fetch(pdfUrl)
+            .then(response => response.blob())
+            .then(blob => {
+                const url = window.URL.createObjectURL(new Blob([blob]));
+                const link = document.createElement('a');
+                link.href = url;
+                link.setAttribute('download', 'HigorVeloso_curriculo.pdf');
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+            })
+            .catch(error => {
+                console.error('Error fetching PDF:', error);
+            });
+    };
     return (
-        <section  className='' style= {{backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: '#f1f5f9'}}>
-              <h1>Em Produção...</h1>
-              <h1>Em Produção...</h1>
-              <h1>Em Produção...</h1>
-              <h1>Em Produção...</h1>
-              
-        </section>        
+        <section className='' style={{ backgroundImage: 'url(/imagens/fundo.png)', backgroundColor: '#f1f5f9' }}>
+            <button onClick={downloadPDF}>Download PDF</button>
+        </section>
     )
 };
 
